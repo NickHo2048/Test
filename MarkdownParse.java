6733f687bff46b134e89dd366eae76ccba242fde
NickHo2048 <niho@ucsd.edu>
2022-02-25 14:17:20 -0800

    lab

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -5,35 +5,74 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MarkdownParse {
-    public static ArrayList<String> getLinks(String markdown) {
+    public static int indexOfUnescaped(String str, String search, int startIndex) {
+        int currIndex = startIndex - 1;
+        int backslashCount;
+        do {
+            currIndex = str.indexOf(search, currIndex + 1);
+            backslashCount = 0;
+            int index = currIndex - 1;
+            while (index >= 0 && str.charAt(index) == '\\') {
+                index--;
+                backslashCount++;
+            }
+        } while (backslashCount % 2 == 1);
+        return currIndex;
+    }
+
+    public static ArrayList<String> getLinksFromLine(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            System.out.format("%d\t%d\t%s\n", currentIndex, nextOpenBracket, toReturn);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            if(nextOpenBracket == -1 || nextCloseBracket == -1
-                  || closeParen == -1 || openParen == -1) {
-                return toReturn;
+        while (currentIndex < markdown.length()) {
+            int nextOpenBracket = indexOfUnescaped(markdown, "[", currentIndex);
+            if (nextOpenBracket == -1) {
+                break;
             }
-            String potentialLink = markdown.substring(openParen + 1, closeParen);
-            if(potentialLink.indexOf(" ") == -1 && potentialLink.indexOf("\n") == -1) {
-                toReturn.add(potentialLink);
-                currentIndex = closeParen + 1;
+            int nextCloseBracket = indexOfUnescaped(markdown, "]", nextOpenBracket);
+            int openParen = indexOfUnescaped(markdown, "(", nextCloseBracket);
+            if (openParen == -1) {
+                break;
             }
-            else {
-                currentIndex = currentIndex + 1;
+            int closeParen = indexOfUnescaped(markdown, ")", openParen);
+            int nextOpenParen = indexOfUnescaped(markdown, "(", openParen + 1);
+            if (nextOpenParen != -1 && closeParen > nextOpenParen) {
+                // Invalid link because there's an open paren before the close paren
+                int openBracket = indexOfUnescaped(markdown, "[", openParen);
+                if (openBracket != -1) {
+                    // Maybe there's another link
+                    currentIndex = openBracket;
+                    continue;
+                } else {
+                    break;
+                }
             }
+            if (closeParen == -1) {
+                break;
+            }
+            // Check that this isn't an image link
+            if (!(nextOpenBracket > 0 && markdown.charAt(nextOpenBracket - 1) == '!')) {
+                String substr = markdown.substring(openParen + 1, closeParen);
+                substr = substr.replaceAll("\\\\(.)", "$1");
+                toReturn.add(substr);
+            }
+            currentIndex = closeParen + 1;
         }
         return toReturn;
     }
+
+    public static ArrayList<String> getLinks(String markdown) {
+        ArrayList<String> toReturn = new ArrayList<>();
+        for (String line : markdown.split("\n")) {
+            toReturn.addAll(getLinksFromLine(line));
+        }
+        return toReturn;
+    }
+
     public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
+        Path fileName = Path.of(args[0]);
+        String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
     }
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -21,29 +21,77 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void getLinks() throws IOException{
-       // Path fileName = Path.of("./test-file.md");
-	   // String contents = Files.readString(fileName);
-        //assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
+    public void testBaseCase() throws IOException {
+        String contents = readFile("test-file.md");
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList<String> Reallinks =
+                new ArrayList<>(List.of("https://something.com", "some-page.html"));
+
+        assertArrayEquals(links.toArray(), Reallinks.toArray());
+
+
     }
 
     @Test
-    public void getLinks1() throws IOException{
-       // Path fileName = Path.of("./test2-file.md");
-	   // String contents = Files.readString(fileName);
-        //assertEquals( List.of("link.com"), MarkdownParse.getLinks(contents));
+    public void testEmptyCase() throws IOException {
+        String contents = readFile("test-empty.md");
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList<String> Reallinks = new ArrayList<>(List.of());
+
+        assertArrayEquals(links.toArray(), Reallinks.toArray());
+
     }
+
+
     @Test
-    public void getLinks2() throws IOException{
-        //Path fileName = Path.of("./test3-file.md");
-	   // String contents = Files.readString(fileName);
-       // assertEquals(List.of("link.com"), MarkdownParse.getLinks(contents));
+    public void testNewLine() throws IOException {
+        String contents = readFile("test-file-newline.md");
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList<String> Reallinks =
+                new ArrayList<>(List.of("https://something.com", "some-page.html"));
+        assertArrayEquals(links.toArray(), Reallinks.toArray());
+
+
     }
+
     @Test
-    public void getLinks3() throws IOException{
-       // Path fileName = Path.of("./test4-file.md");
-	    //String contents = Files.readString(fileName);
-        //assertEquals(List.of(), MarkdownParse.getLinks(contents));
+    public void testBackSlash() throws IOException {
+        String contents = readFile("test-backslash-escapes.md");
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList<String> Reallinks = new ArrayList<>(List.of("/close_bracket", "/single_)bracket",
+                "/double_\\", "/triple_\\)bracket", "/quad_\\\\", "/open_(paren"));
+        assertArrayEquals(links.toArray(), Reallinks.toArray());
+
+    }
+
+    @Test
+    public void testEndingText() throws IOException {
+        String contents = readFile("test-ending-text.md");
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList<String> Reallinks =
+                new ArrayList<>(List.of("https://something.com", "some-page.html"));
+        assertArrayEquals(links.toArray(), Reallinks.toArray());
+
+    }
+
+    @Test
+    public void testIgnoreImage() throws IOException {
+        String contents = readFile("test-ignore-image.md");
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList<String> Reallinks =
+                new ArrayList<>(List.of("/actual_link1", "/actual_link2", "/actual_link3"));
+        assertArrayEquals(links.toArray(), Reallinks.toArray());
+
+    }
+
+    @Test
+    public void testBackSlash2() throws IOException {
+        String contents = readFile("test-backslash-escapes2.md");
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList<String> Reallinks = new ArrayList<>(List.of("/close_bracket", "/single_)bracket",
+                "/double_\\", "/triple_\\)bracket", "/quad_\\\\", "/open_(paren"));
+        assertArrayEquals(links.toArray(), Reallinks.toArray());
+
     }
     @Test
     public void testSnippet1() throws IOException
